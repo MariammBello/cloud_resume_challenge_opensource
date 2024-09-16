57a053f1d474c3b2350b257775265b0f0b433c1b
Mariam Bello <sheactuary@gmail.com>
2024-09-16 20:17:37 +0100

    automating deployment in github actions

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+echo "Stopping and removing old frontend containers..."
+docker stop frontend
+docker rm frontend
+
+# Pull the latest images from GitHub Container Registry
+echo "Pulling latest frontend images from GHCR..."
+docker pull ghcr.io/your-gh-username/resume-frontend:latest
+
+
+# Run the Frontend container
+echo "Starting Frontend container..."
+docker run -d --name frontend -p 80:80 ghcr.io/your-gh-username/resume-frontend:latest
+
+echo "Update complete!"
