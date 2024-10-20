fffaf392e183a3b114ae1ff3cd07dfb21cbb1e5c
mariammbello <sheactuary@gmail.com>
2024-10-20 20:41:27 +0100

    updating dockercompose context ref

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Stop and remove old containers
 echo "Stopping and removing old containers..."
-docker-compose down || true
+docker compose down || true
 
 # Pull latest images from GHCR
 echo "Pulling latest images from GHCR..."
@@ -11,7 +11,7 @@ docker pull ghcr.io/mariammbello/resume-backend:latest
 
 # Start everything using Docker Compose
 echo "Starting containers..."
-docker-compose up -d
+docker compose up -d
 
 echo "Update complete!"
  
